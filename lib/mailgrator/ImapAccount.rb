ed561690382e87da565754b7763c966f584898e7
spox <spox@bang.(none)>
2009-03-03 11:15:30 -0800

    added some documentation

--- lib/mailgrator/ImapAccount.rb
+++ lib/mailgrator/ImapAccount.rb
@@ -1,7 +1,15 @@
 ['MailAccount', 'MailboxListImap', 'MailConnection'].each{|f| require "mailgrator/#{f}"}
 
 module MailGrator
+
+    # Used for imap mail interaction
     class ImapAccount < MailAccount
+        # server:: server name to connect to
+        # username:: username to use for connection
+        # password:: password to use for connection
+        # port:: port the server is listening on
+        # secure:: use a secure connection
+        # Creates a new ImapAccount
         def initialize(server, username, password, port=143, secure=false)
             @connection = MailConnection.new(server, username, password, port, secure)
             @mailbox_list build_mailboxes
