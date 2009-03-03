20b522ecbb132c1a5efc86e64d39af2ff11791ca
spox <spox@bang.(none)>
2009-03-03 11:15:56 -0800

    added some documentation

--- lib/mailgrator/MboxAccount.rb
+++ lib/mailgrator/MboxAccount.rb
@@ -1,8 +1,10 @@
 ['MailAccount', 'MailboxListMbox'].each{|f| require "mailgrator/#{f}"}
 
 module MailGrator
+    # Used for mbox mail interaction
     class MboxAccount < MailAccount
-
+        # path:: Path to mbox file
+        # Creates a new MboxAccount
         def initialize(path)
             @path = path
             @mailbox_list = build_mailboxes
